830e7f585c96fa871abd8dbf90687c6765b38b18
TurbogamesDev <turbogames.mail@gmail.com>
2026-06-06 22:29:31 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import utils.graphql_interface as graphql_interface
+
+test_query_file_path = "queries/test_query.graphql"
+
+test_query = graphql_interface.get_query_from_file_path(test_query_file_path)
+
+response = graphql_interface.run_anilist_query(
+    test_query,
+    {
+        "type": "ANIME",
+        "userName": "turbogames",
+        "status": "CURRENT"
+    }
+)
+
+for entry in response["MediaListCollection"]["lists"][0]["entries"]:
+    entry_title = entry["media"]["title"]["english"]
+    entry_progress = entry["progress"]
+    entry_episodes = entry["media"]["episodes"]
+
+    if entry_episodes == None:
+        entry_episodes = max(entry_progress, 12)
+
+    print(f"{entry_title} ({entry_progress}/{entry_episodes})")
+
+
+
